6b62dd6d458505b17d22962c2bb574a22eb4e248
vitor <vitorduarte.vds7@gmail.com>
2023-01-10 20:57:23 -0300

    api requests and navbar

---
+++ utils/requests.js
@@ -0,0 +1,36 @@
+const API_KEY = process.env.API_KEY;
+
+export default {
+    fetchTrending: {
+        title: "Trending",
+        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    },
+    fetchTopRated: {
+        title: "Top Rated",
+        url: `/movie/top_rated?api_key=${API_KEY}$language=en-US`,
+    },
+    fetchActionMovies: {
+        title: "Action",
+        url: `/discover/movie?api_key=${API_KEY}$with_genres=28`,
+    },
+    fetchComedyMovies: {
+        title: "Comedy",
+        url: `/discover/movie?api_key=${API_KEY}$with_genres=35`,
+    },
+    fetchAHorrorMovies: {
+        title: "Horror",
+        url: `/discover/movie?api_key=${API_KEY}$with_genres=27`,
+    },
+    fetchARomanceMovies: {
+        title: "Romance",
+        url: `/discover/movie?api_key=${API_KEY}$with_genres=10749`,
+    },
+    fetchAMysteryMovies: {
+        title: "Mystery",
+        url: `/discover/movie?api_key=${API_KEY}$with_genres=9648`,
+    },
+    fetchASciFiMovies: {
+        title: "Sci-Fi",
+        url: `/discover/movie?api_key=${API_KEY}$with_genres=878`,
+    },
+};
