82b6e84e559143c1d020253577f72b6d52bcf698
Vitor Duarte <vitorduarte.vds7@gmail.com>
2023-01-11 16:45:58 -0300

    aa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
-import Header from "../components/Header"
-import { Nav } from "../components/Nav"
-export default function Home() {
-  return (
-    <div>
-      <title>hulu</title>
-      <Header />
-      <Nav />
-    </div>
-  )
+import Header from "../components/Header";
+import { Nav } from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
+
+export default function Home({ results }: any) {
+    console.log(results);
+    return (
+        <div>
+            <title>hulu</title>
+            <Header />
+            <Nav />
+            <Results results={results} />
+        </div>
+    );
+}
+
+export async function getServerSideProps(context: any) {
+    const genre = context.query.genre;
+    const request = await fetch(
+        `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url
+        }`
+    ).then((res) => res.json());
+    return {
+        props: { results: request.results },
+    };
 }
